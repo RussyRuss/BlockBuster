4f7082e610d93893fefc800f15b760284554de98
russy <russy@DESKTOP-C0A109U>
2021-02-08 00:41:31 -0500

    Add project files.

---
+++ BlockBuster/BlockBusterBasicFunctions.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using BlockBuster.Models;
+
+namespace BlockBuster
+{
+
+    public class BlockBusterBasicFunctions
+    {
+        public static Movie GetMovieById(int id)
+        {
+            using(var db = new SE407_blockBusterContext())
+            {
+                return db.Movies.Find(id);
+            }
+        }
+
+        public static List<Movie> GetAllMovies()
+        {
+            using(var db = new SE407_blockBusterContext())
+            {
+                return db.Movies.ToList();
+            }
+        }
+        
+        public static List<Director> GetDirectorsLast()
+        {
+            using (var db = new SE407_blockBusterContext())
+            {
+                Director.Select(m => m.LastName).Where(m => m == "LastName");
+                return db.Directors.ToList();
+            }
+        }
+
+        public static List<Genre> GetGenres()
+        {
+            using (var db = new SE407_blockBusterContext())
+            {
+                Genre.Select(m => m.GenreDescr).Where(m => m == "GenreDescr");
+                return db.Genre.ToList();
+            }
+        }
+        public static List<Movie> GetAllCheckedOutMovies()
+        {
+            using (var db = new SE407_blockBusterContext())
+            {
+                return db.Movies
+                    .Join(db.Transactions,
+                    m => m.MovieId,
+                    t => t.Movie.MovieId,
+                    (m, t) => new
+                    {
+                        MovieId = m.MovieId,
+                        Title = m.Title,
+                        ReleaseYear = m.ReleaseYear,
+                        GenreId = m.GenreId,
+                        DirectorId = m.DirectorId,
+                        CheckedIn = t.CheckedIn
+
+                    }).Where(w => w.CheckedIn == "N")
+                    .Select(m => new Movie
+                    {
+                        MovieId = m.MovieId,
+                        Title = m.Title,
+                        ReleaseYear = m.ReleaseYear,
+                        GenreId = m.GenreId,
+                        DirectorId = m.DirectorId,
+
+                    }).ToList();
+
+            }
+        }
+     
+        
+
+
+
+
+
+
+    }
+}
---
+++ BlockBusterTest/BlockBusterBasicFunctionsTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BlockBuster;
+using BlockBusterTest;
+using Xunit;
+
+namespace BlockBusterTest
+{
+
+    public class BlockBusterBasicFunctionsTest
+    {
+        [Fact]
+        public void GetMovieByIdTest()
+        {
+            var result = BlockBusterBasicFunctions.GetMovieById(11);
+            Assert.True(result.Title == "Vertigo");
+            Assert.True(result.ReleaseYear == 1958);
+        }
+        [Fact]
+        public void GetAllMovies()
+        {
+            var result = BlockBusterBasicFunctions.GetAllMovies();
+            Assert.True(result.Count == 50);
+        }
+
+        [Fact]
+        public void GetAllCheckedOutMovies()
+        {
+            var result = BlockBusterBasicFunctions.GetAllCheckedOutMovies();
+            Assert.True(result.Count == 3);
+        }
+        [Fact]
+        public void GetGenres()
+        {
+            var result = BlockBusterBasicFunctions.GetGenre(11);
+            Assert.True(result.GenreDescr == "Vertigo");
+          
+        }
+        [Fact]
+        public void GetDirectorsLast()
+        {
+            var result = BlockBusterBasicFunctions.GetDirectorsLast();
+            Assert.True(result.Count == 50);
+        }
+
+
+
+
+
+
+    }
+
+
+}
+
+    
